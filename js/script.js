91f45856f427210cec3c443e3e7d1a740aa97d46
Leticia Cabral <leticiarosario67@gmail.com>
2023-03-06 16:00:32 -0100

    firt commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+function toogleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+  //pegar a tag img
+  const img = document.querySelector("#profile img")
+  //substituir a imagem
+  if (html.classList.contains("light")) {
+    //se tiver light mode, adicionar a imagem light
+    img.setAttribute("src", "./assets/Avatar-light.png")
+  } else {
+    img.setAttribute("src", "./assets/Avatar.png")
+  }
+}
